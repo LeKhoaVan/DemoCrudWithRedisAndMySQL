cbbda1deef20b677f18c7d33c8f7762824bc5637
LeKhoaVan <lekhoavan325802@gmail.com>
2022-10-31 11:22:30 +0700

    Initial commit

---
+++ src/main/java/iuh/dhktpm15b/DemoCrudWithRedisAndMySQL/DemoCrudWithRedisAndMySqlApplication.java
@@ -0,0 +1,13 @@
+package iuh.dhktpm15b.DemoCrudWithRedisAndMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudWithRedisAndMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudWithRedisAndMySqlApplication.class, args);
+	}
+
+}
